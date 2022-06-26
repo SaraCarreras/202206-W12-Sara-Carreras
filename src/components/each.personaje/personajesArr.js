ad9cc4fddb775c8124e23a08d3a1b00deb2dfda2
Sara Carreras <saracc621@gmail.com>
2022-06-26 20:28:48 +0200

    creating characters array

---
+++ src/components/each.personaje/personajesArr.js
@@ -0,0 +1,47 @@
+//crear array de objetos PErsonajes aquí son su nombre,el ID y su special skill que varías egún el tpo de personaje
+//no sé si  con un array dentro  o sino a traves del interfaz, PERSONAJE y luego a parte
+// en el mostrat que sea CONDICIONAL,si tiene la prop reinado s emuestre y sino no.
+export const PersonajesArr = [
+  {
+    id: 1,
+    name: 'Joffrey Baratheon',
+    typeOfCharacter: 'king',
+    yearsOfReign: 1.5,
+    age: 16,
+    emoji: '👑',
+  },
+  {
+    id: 2,
+    name: 'Jaime Lannister',
+    typeOfCharacter: 'fighter',
+    weapon: 'sword',
+    skill: 7,
+    age: 32,
+    emoji: '🗡',
+  },
+  {
+    id: 3,
+    name: 'Daenerys Targaryen ',
+    typeOfCharacter: 'fighter',
+    weapon: 'dragons',
+    skill: 9,
+    age: 20,
+  },
+  {
+    id: 4,
+    name: 'Tyrion Lannister',
+    typeOfCharacter: 'adviser',
+    adviserOf: 'Daenerys',
+    age: 26,
+    emoji: '🗡',
+  },
+  {
+    id: 5,
+    name: 'Bronn Stokeworth',
+    typeOfCharacter: 'squire',
+    squirerOf: 'Jaime Lannister',
+    gradeOfSubmission: '9',
+    age: 41,
+    emoji: '🛡',
+  },
+];
