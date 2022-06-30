dcd3d11ec78b72ade15fcf6def58293040e8275d
Sara Carreras <saracc621@gmail.com>
2022-06-30 12:57:53 +0200

    setting up test card

---
+++ src/components/personajes.card/personajes.card.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react';
+import { PersonajesCard } from './personajes.card';
+import { iPersonajesArr } from '../interfaces/personajes.arr';
+import userEvent from '@testing-library/user-event';
+
+describe('Given the component PersonajesCard', () => {
+  describe('when the component is rendered', () => {
+    test("should render a Personaje's card with this info", () => {
+      const PersonajesArr: iPersonajesArr = {
+        id: 1,
+        alive: true,
+        imgName: 'Joffrey',
+        nameAndFamily: 'Test',
+        typeOfCharacter: 'king',
+        yearsOfReign: 1.5,
+        age: 16,
+        emoji: '👑',
+        phrase: 'Vais a morir todos',
+      };
+
+      render(
+        <PersonajesCard
+          card={PersonajesArr}
+          toDie={() => {}}
+          toSpeack={() => {}}
+        ></PersonajesCard>
+      );
+      const elementArr = screen.getByText(/Edad: 16 años/i);
+      expect(elementArr).toBeInTheDocument();
+    });
+
+    describe('when the button is clicked', () => {
+      test('should kill a character', () => {
+        const PersonajesArr: iPersonajesArr = {
+          id: 1,
+          alive: true,
+          imgName: 'Joffrey',
+          nameAndFamily: 'Test',
+          typeOfCharacter: 'king',
+          yearsOfReign: 1.5,
+          age: 16,
+          emoji: '👑',
+          phrase: 'Vais a morir todos',
+        };
+        const deleteMock = jest.fn();
+        render(
+          <PersonajesCard
+            card={PersonajesArr}
+            toDie={deleteMock}
+            toSpeack={() => {}}
+          ></PersonajesCard>
+        );
+
+        const deletebutton = screen.queryAllByTestId('button-delete');
+        userEvent.click(deletebutton[0]);
+        expect(deleteMock).toHaveBeenCalled();
+      });
+    });
+  });
+});
